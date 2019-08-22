d407b8ad74d83e172b2cc0b37332bd9a489fe359
mkaczor <mateusz.w.kaczor@gmail.com>
2019-08-22 09:24:41 +0200

    Add Zuul proxy.

--- Zuul/src/main/java/com/luxoft/training/spring/cloud/Zuul.java
+++ Zuul/src/main/java/com/luxoft/training/spring/cloud/Zuul.java
@@ -1,4 +1,16 @@
 package com.luxoft.training.spring.cloud;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableZuulProxy
+@EnableEurekaClient
 public class Zuul {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Zuul.class, args);
+    }
 }
